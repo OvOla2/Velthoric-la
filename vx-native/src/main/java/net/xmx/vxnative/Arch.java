85c24604385bfa9e9bd0658bd810237519a52afe
OvOla2 <lsr1024@qq.com>
2025-10-12 14:52:01 +0800

    Update Arch.java
    fixed variable issue

--- vx-native/src/main/java/net/xmx/vxnative/Arch.java
+++ vx-native/src/main/java/net/xmx/vxnative/Arch.java
@@ -8,7 +8,7 @@ import java.util.Locale;
 
 enum Arch {
         X86_64("x86-64"),
-        AARCH64("aarch64");
+        AARCH64("aarch64"),
         LOONGARCH64("loongarch64");
         
         final String folder;
